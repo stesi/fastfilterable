c9804069ce279df76c6a9bd06a58c21b9d44c059
Enzo <diroberto.v@stesi.eu>
2016-07-19 11:56:24 +0200

    Select with comma separator

--- PropelBehavior/templates/objectUpdateFastFilterable.php
+++ PropelBehavior/templates/objectUpdateFastFilterable.php
@@ -5,73 +5,88 @@
  *
  * @return     \$this|$queryClassName La query filtrata
  */
-    public function fastFilter($filtri) {
-            $filtriElaborati = array ();
-            foreach ( $filtri as $filtro => $value ) {
-            	if (!empty($value))
-            	{
-            	$name=explode("_",$filtro);
-            	$type=$name[0];
-            	array_shift($name);
-            	$filtro=implode("_",$name);
-            	switch($type){
-            		
-            		case "TextBox":
-            			$primoCarettere = substr ( $value, 0, 1 );
-            			$ultimoCarattere = substr ( $value, - 1 );
-            			if ($primoCarettere == '%' or $ultimoCarattere == '%') {
-            				$filtriElaborati [$filtro] ['operatore'] = "like";
-            			} else {
-            				$filtriElaborati [$filtro] ['operatore'] = "=";
-            			}
-            			$filtriElaborati [$filtro] ['valore'] = $value;
-            			break;
-            		case "Date":
-            			$date=explode("/",$value);
-            			if(count($date)==2){
-            				$filtriElaborati [$filtro] ['operatore'] = "BETWEEN";
-            				$filtriElaborati [$filtro]['valore']=$date;
-            			}else if(count($date)==1){
-            				$filtriElaborati [$filtro] ['operatore'] = "=";
-            				$filtriElaborati [$filtro]['valore']=implode($date);
-            			}
-            			break;
-            		case "Select":            
-            			if(!is_array($value))
-            				$value=explode(",",$value);
-            			if (is_array ( $value )) {
-            				$filtriElaborati [$filtro] ['operatore'] = "IN";
-            				$filtriElaborati [$filtro] ['valore'] = $value;
-            			} else {
-            				$filtriElaborati [$filtro] ['operatore'] = "=";
-            				$filtriElaborati [$filtro] ['valore'] = $value;
-            			}
-            			break;
-            		
-            	}
-            	}
-            }
-            if (count($filtriElaborati)>0)
-            {
-            $i = 0;
-            $conditionArray = array();
-            foreach ( $filtriElaborati as $filtro => $value ) {
-                $i++;
-                $conditionName = "fastFilter".$i;
-                $conditionArray[] = $conditionName;
-                if($value['operatore']=="BETWEEN"){
-                	$this->condition( $conditionName,$filtro . " >=" . "?", $value ['valore'][0] );
-                	$conditionArray[] = $conditionName."_".$i;
-                	$this->condition( $conditionName."_".$i,$filtro . " <=" . "?", $value ['valore'][1] );
+       public function fastFilter($filtri) {
+                $filtriElaborati = array ();
+                foreach ( $filtri as $filtro => $value ) {
+                    if (!empty($value))
+                    {
+                    $name=explode("_",$filtro);
+                    $type=$name[0];
+                    array_shift($name);
+                    $filtro=implode("_",$name);
+                    switch($type){
+                        
+                        case "TextBox":
+                            $primoCarettere = substr ( $value, 0, 1 );
+                            $ultimoCarattere = substr ( $value, - 1 );
+                            if ($primoCarettere == '%' or $ultimoCarattere == '%') {
+                                $filtriElaborati [$filtro] ['operatore'] = "like";
+                            } else {
+                                $filtriElaborati [$filtro] ['operatore'] = "=";
+                            }
+                            $filtriElaborati [$filtro] ['valore'] = $value;
+                            break;
+                        case "Date":
+                            $date=explode("/",$value);
+                            if(count($date)==2){
+                                $filtriElaborati [$filtro] ['operatore'] = "BETWEEN";
+                                $filtriElaborati [$filtro]['valore']=$date;
+                            }else if(count($date)==1){
+                                $filtriElaborati [$filtro] ['operatore'] = "=";
+                                $filtriElaborati [$filtro]['valore']=date( 'Y-m-d', strtotime(implode($date)) );
+                            }
+                            break;
+                        case "Select":            
+                            if(!is_array($value))
+                                $value=explode(",",$value);
+                            if (is_array ( $value )) {
+                            	$values=array();
+                            	foreach($value as $v){
+                            		if(!empty($v)){
+                            			$v=explode(",",$v);
+                            			if(is_array($v))
+                            				$values=array_merge($values,$v);
+                            			else
+                            				$values[]=$v;
+                            		}
+                            	}
+                            	if(count($values)>0){
+                                	$filtriElaborati [$filtro] ['operatore'] = "IN";
+                                	$filtriElaborati [$filtro] ['valore'] = $values;
+                            	}
+                            } else {
+                                $filtriElaborati [$filtro] ['operatore'] = "=";
+                                $filtriElaborati [$filtro] ['valore'] = $value;
+                            }
+                            break;
+                        
+                    }
+                    }
                 }
-                else{
-                	$this->condition( $conditionName, $filtro . " " . $value ['operatore'] . " ?", $value ['valore'] );
+                if (count($filtriElaborati)>0)
+                {
+                $i = 0;
+                $conditionArray = array();
+                foreach ( $filtriElaborati as $filtro => $value ) {
+                    $i++;
+                    $conditionName = "fastFilter".$i;
+                    $conditionArray[] = $conditionName;
+                    if($value['operatore']=="BETWEEN"){
+                        $this->condition( $conditionName,$filtro . " >=" . "?", date( 'Y-m-d', strtotime($date[0]) )." 00:00:00") ;
+                        $i++;
+                        $conditionArray[] = $conditionName."_".$i;
+                        $this->condition( $conditionName."_".$i,$filtro . " <=" . "?", date( 'Y-m-d', strtotime($date[1]) )." 23:59:59") ;
+                    }
+                    else{
+                        $this->condition( $conditionName, $filtro . " " . $value ['operatore'] . " ?", $value ['valore'] );
+                    }
                 }
+                $this -> where($conditionArray,'and');
+                }
+                return $this;
             }
-            $this -> where($conditionArray,'and');
-            }
-            return $this;
-        }
+      
+ 
   
     
 public function fastWith($withs) {
